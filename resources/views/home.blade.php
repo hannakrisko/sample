1ee41b696a5ba12dcb7368bbf34620ea29832840
hannakrisko <hannakrissko@gmail.com>
2017-12-07 19:30:12 +0800

    added new project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.app')
+
+@section('content')
+<h1>Home</h1>
+@endsection
+
+@section('sidebar')
+  @parent
+<p>This is appended to sidebar</p>
+@endsection
---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <meta charset="utf-8">
+      <title></title>
+      <link rel="stylesheet" href="/css/app.css">
+    </head>
+    <body>
+      @yield('content')
+
+      @include('inc.sidebar')
+
+    </body>
+</html>
